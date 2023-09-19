bb96da9590ae4c99593a17828a69c40fab6d6ddd
Felipe Palazzo <palazzofelipe@gmail.com>
2023-09-19 21:09:00 +0100

    add admin scripts

--- includes/class-picnote.php
+++ includes/class-picnote.php
@@ -27,6 +27,12 @@ class Picnote_Plugin
         add_menu_page('Picnote Settings', 'Picnote', 'manage_options', $this->plugin_name, array($this, 'picnote_admin_page_html'), 'dashicons-cover-image');
     }
 
+    function enqueue_admin_scripts()
+    {
+        wp_enqueue_media();
+        wp_enqueue_script($this->plugin_name . 'admin-scripts', plugin_dir_path(dirname(__FILE__)) . 'js/picnote-admin.js', array('jquery', 'thickbox', 'media-upload'), $this->version, false);
+    }
+
     function picnote_admin_page_html()
     {
         require_once plugin_dir_path(dirname(__FILE__)) . 'views/admin-picnote.php';
@@ -35,5 +41,6 @@ class Picnote_Plugin
     private function define_admin_hooks()
     {
         add_action('admin_menu', array($this, 'picnote_admin_page'));
+        add_action('admin_enqueue_scripts', array($this, 'enqueue_admin_scripts'));
     }
 }
--- views/admin-picnote.php
+++ views/admin-picnote.php
@@ -1,3 +1,4 @@
 <?php
 ?>
 <h1>hello world ...</h1>
+<a href="#" class="button button-primary" id="add-image-btn">Add image</a>
